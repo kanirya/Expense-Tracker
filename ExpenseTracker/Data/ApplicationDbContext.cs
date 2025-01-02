c7472fa6f8114308e492264a465209d266a1b629
Muhammad <danishkanirya9999@gmail.com>
2025-01-03 03:28:42 +0500

    Adding Controller and Models

---
+++ ExpenseTracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ExpenseTracker.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
