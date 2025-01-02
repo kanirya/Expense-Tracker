82be7259d48e4297e733b14b7f19c7aea8fd8064
Muhammad <danishkanirya9999@gmail.com>
2025-01-03 04:24:41 +0500

    Making endpoints of crud operations

--- ExpenseTracker/Controllers/EmployeeController.cs
+++ ExpenseTracker/Controllers/EmployeeController.cs
@@ -1,11 +1,76 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Expense_Tracker.Models;
+using ExpenseTracker.Models;
+using ExpenseTracker.Models.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ExpenseTracker.Controllers
 {
+    //localhost:xxxx/api/employees
     [Route("api/[controller]")]
     [ApiController]
     public class EmployeeController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeeController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+          var allEmployees=  _context.Employees.ToList();
+            return Ok(allEmployees);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDto addEmployeeDto)
+        {
+            var employeeEntity = new Employee()
+            {
+                Name = addEmployeeDto.Name,
+                Email = addEmployeeDto.Email,
+                Phone = addEmployeeDto.Phone,
+                Salary = addEmployeeDto.Salary
+            };
+
+
+            _context.Employees.Add(employeeEntity);
+            _context.SaveChanges();
+
+            return Ok(employeeEntity);
+        }
+
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetEmployeeId(Guid id) {
+         var employee=   _context.Employees.Find(id);
+            if(employee is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult UpdateEmployee(Guid id,UpdateEmployeeDto updateEmployeeDto)
+        {
+            var employee = _context.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            employee.Name = updateEmployeeDto.Name;
+            employee.Email = updateEmployeeDto.Email;
+            employee.Phone = updateEmployeeDto.Phone;
+            employee.Salary = updateEmployeeDto.Salary;
+
+            _context.SaveChanges();
+            return Ok(employee);
+        }
     }
 }
